dbbd8131b40eb958ec5d2cac7e54c1993ee44455
hajunk05 <hajunk05@gmail.com>
2025-10-03 18:49:48 +1300

    Fetching random verse and basic rendering

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,54 @@
+import { useEffect, useState } from 'react'
+
 const Home = () => {
-	return <> home </>
+	const [verseData, setVerseData] = useState('')
+	const [error, setError] = useState('')
+
+	useEffect(() => {
+		randomVerse()
+	}, [])
+
+	const randomVerse = async () => {
+		try {
+			const response = await fetch(
+				'https://bible-api.com/data/web/random'
+			)
+
+			if (!response.ok) {
+				throw new Error('Failed to fetch verse')
+			}
+
+			const fullVerseData = await response.json()
+
+			const { name } = fullVerseData.translation
+			const { book, book_id, chapter, text, verse } =
+				fullVerseData.random_verse
+
+			const randomVerse = {
+				name,
+				book,
+				book_id,
+				chapter,
+				text,
+				verse,
+			}
+			setVerseData(randomVerse)
+		} catch (e) {
+			setError(e.message)
+		}
+	}
+
+	return (
+		<div>
+			<h1> From: {verseData.name}</h1>
+			<h2> {verseData.book} </h2>
+			<p>
+				{' '}
+				{verseData.chapter}:{verseData.verse}{' '}
+			</p>
+			<p> {verseData.text}</p>
+		</div>
+	)
 }
 
 export default Home
