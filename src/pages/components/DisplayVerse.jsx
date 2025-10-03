59a2a3b99b0928d50bedea77cedf5dba0cdddb6b
hajunk05 <hajunk05@gmail.com>
2025-10-03 21:40:40 +1300

    Some basic css for the home page

--- src/pages/components/DisplayVerse.jsx
+++ src/pages/components/DisplayVerse.jsx
@@ -7,25 +7,30 @@ const DisplayVerse = ({
 	isAlreadyFav,
 }) => {
 	return (
-		<div class="display-container">
+		<div className="display-container">
 			<h1> From: {currentVerseData.name}</h1>
-			<h2> {currentVerseData.book} </h2>
-			<p>
-				{' '}
-				{currentVerseData.chapter}:{currentVerseData.verse}{' '}
-			</p>
-			<p> {currentVerseData.text}</p>
-			<button onClick={() => handleRandomVerse()}>
-				{' '}
-				Another Verse{' '}
-			</button>
-			{isMaxFav || isAlreadyFav ? (
-				<button disabled> Add to Favorites </button>
-			) : (
-				<button onClick={() => handleAddToFavorites()}>
-					Add to Favorites
+			<div className="display-text-only">
+				<h2> {currentVerseData.book} </h2>
+				<p>
+					{' '}
+					{currentVerseData.chapter}:
+					{currentVerseData.verse}{' '}
+				</p>
+				<p> {currentVerseData.text}</p>
+			</div>
+			<div className="display-buttons-only">
+				<button onClick={() => handleRandomVerse()}>
+					{' '}
+					Another Verse{' '}
 				</button>
-			)}
+				{isMaxFav || isAlreadyFav ? (
+					<button disabled> Add to Favorites </button>
+				) : (
+					<button onClick={() => handleAddToFavorites()}>
+						Add to Favorites
+					</button>
+				)}
+			</div>
 		</div>
 	)
 }
