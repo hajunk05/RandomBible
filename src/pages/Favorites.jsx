97ab9afd14cf8d98a274e18855a9f9e1fc025458
hajunk05 <hajunk05@gmail.com>
2025-10-03 19:24:45 +1300

    Simple component for parsing of fav verses

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,5 +1,21 @@
 const Favorites = ({ favVerses, setFavVerses }) => {
-	return <> {} </>
+	return (
+		<>
+			{favVerses.map((verseData) => {
+				return (
+					<div style={{ border: '1px solid' }}>
+						<h1> {verseData.name} </h1>
+						<h2> {verseData.book} </h2>
+						<p>
+							{' '}
+							{verseData.chapter}:{verseData.verse}
+						</p>
+						<p> {verseData.text} </p>
+					</div>
+				)
+			})}
+		</>
+	)
 }
 
 export default Favorites
